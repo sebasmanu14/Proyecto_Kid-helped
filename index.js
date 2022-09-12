9e99fc23087d5df7e90135d102f14afc9dc1e7c3
Alexander Bravo <vaj.bravo@yavirac.edu.ec>
2022-09-12 17:11:09 -0500

    actualizado

--- index.js
+++ index.js
@@ -14,41 +14,60 @@ app.get("/principal", (req, res) => {
   res.render("core/principal", {
     style: "principal.css",
   });
-}); 
+});
 
 app.get("/adultos", (req, res) => {
   res.render("core/adultos", {
     style: "adultos.css",
-  }); 
-});    
+  });
+});
 app.get("/adulto", (req, res) => {
   res.render("core/adulto", {
-    style: "adultos2.css", 
-  }); 
-});      
- 
+    style: "adultos2.css",
+  });
+});
+
 app.get("/informacion", (req, res) => {
   res.render("core/informacion", {
     style: "informacion.css",
-  }); 
-});   
-   
+  });
+});
+
 app.get("/register", (req, res) => {
   res.render("auth/register", {
     style: "register.css",
   });
 });
- 
+
 app.get("/login", (req, res) => {
   res.render("auth/login", {
     style: "login.css",
   });
 });
-  
+ 
 app.get("**", (req, res) => {
   res.render("common/not-found");
-}); 
-
+});
+ 
 //inicio servidor
 app.listen(port, () => console.log(`App listening to port ${port}`));
-       
+
+//Conexion Base de Datos
+var mysql=require('mysql'); 
+ 
+var conexion=mysql.createConnection({ 
+    host:'localhost', 
+    database:'log_in', 
+    user:'root', 
+    password:'' 
+}); 
+ 
+conexion.connect(function(error){ 
+    if(error){ 
+        throw error;    
+    }else{ 
+        console.log('conexion exitosa'); 
+    } 
+}); 
+
+conexion.end();
